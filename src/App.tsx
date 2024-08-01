da223f94effdaa43f4481592824e5243eb27e86a
Anton Kazaryan <kazarian.anton.wp@mail.ru>
2024-08-01 21:38:01 +0300

    init app

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import Header from "./components/Header";
+
+export default function App() {
+  return (
+    <>
+      <Header/>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </>
+  );
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,5 @@
+export default function Header() {
+  return (
+    <h1 className="text-3xl font-bold underline">Header</h1>
+  )
+}
